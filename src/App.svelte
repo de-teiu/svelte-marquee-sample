3bfa1c7f10d03dcffc6292888e3aab12cec2da46
LAPTOP-SGEIJU4B\tekur <tekuragari@gmail.com>
2021-01-15 20:49:18 +0900

    初回コミット

---
+++ src/App.svelte
@@ -0,0 +1,10 @@
+<script>
+	import Marquee from "./Marquee.svelte";
+</script>
+
+<main>
+	<Marquee text="こんな感じで文字が流れます。" />
+</main>
+
+<style>
+</style>
---
+++ src/Marquee.svelte
@@ -0,0 +1,46 @@
+<script>
+  import { onMount } from "svelte";
+  export let text;
+  let style = "";
+  const resize = () => {
+    const duration = Math.trunc(window.innerWidth / 60) + text.length / 10;
+    const textWidth = text.length * 16;
+    style = `animation-duration:${duration}s;` + `width:${textWidth}px;`;
+  };
+
+  onMount(() => {
+    resize();
+  });
+</script>
+
+<style>
+  .marquee {
+    width: 100vw;
+    overflow: hidden;
+  }
+
+  .marquee_text {
+    padding-left: 100%;
+    text-align: left;
+    white-space: nowrap;
+
+    animation-name: marquee;
+    animation-timing-function: linear;
+    animation-iteration-count: infinite;
+  }
+
+  @keyframes marquee {
+    from {
+      transform: translate(0%);
+    }
+    100%,
+    to {
+      transform: translate(-100%);
+    }
+  }
+</style>
+
+<svelte:window on:resize={resize} />
+<div class="marquee">
+  <div class="marquee_text" {style}>{text}</div>
+</div>
